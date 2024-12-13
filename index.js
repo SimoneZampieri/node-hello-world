375cf6b378f91eb54c1cdc75c2da75b08deb0054
SimoneZampieri <simonezampieri106@gmail.com>
2024-12-13 15:06:02 +0100

    [end] esercizio completo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+console.log("Hello Boolean!");
+
+
+//faccio in modo che js accetti un argomento 
+
+
+//prendo gli argomenti dal terminale
+const args = process.argv.slice(2);
+
+//stampo ''World'' se non c'è nulla
+const name = args[0] || 'World';
+
+console.log(`Hello ${name}`);
